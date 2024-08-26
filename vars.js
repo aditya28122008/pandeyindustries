bc5936a42a28fae545af66f1cbc9f4381b47a2a8
Aditya Pandey <117929914+aditya28122008@users.noreply.github.com>
2024-08-26 17:58:29 +0530

    A Fix

--- vars.js
+++ vars.js
@@ -1,4 +1,4 @@
-const upload_preset = "pandeyIndustries_dev"
+const upload_preset = `${process.env.UPLOAD_PRESET}`
 const GITHUB_ID = `${process.env.GITHUB_ID}`
 const GITHUB_SECRET = `${process.env.GITHUB_SECRET}`
 const GOOGLE_CLIENT_ID =`${process.env.GOOGLE_CLIENT_ID}`
